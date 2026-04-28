7db0d650f1976c4d1e512e9009f7da88ed962a94
nnttfresrag-coder <nntt.fres.rag@gmail.com>
2026-04-28 22:40:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
+ return {
+  base: '/cibersegura-tu-guia-digital/',
+  plugins: [react(), tailwindcss()],
+  define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
